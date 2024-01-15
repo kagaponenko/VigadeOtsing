774c55b62cab53c490a9547348810c902c5d6f76
Ekaterina Gaponenko <Ekaterina.Gaponenko@tthkee.onmicrosoft.com>
2024-01-15 21:19:29 +0200

    aaaa

--- VigadeOtsing.py
+++ VigadeOtsing.py
@@ -1,29 +1,28 @@
-
 from math import *  #неправильная расстановка слов
 print("Ruudu karakteristikud")
 a=float(input("Sisesta ruudu külje pikkus => ")) #float
 S=a**2
-print("Ruudu pindala", round(S,1)) 
+print("Ruudu pindala", round(S,1)) #(округление)
 P=4*a
-print("Ruudu umbermoot", round(P,1)) #неправильные кавычки
-di=a*sqrt(2) 
+print("Ruudu umbermoot", round(P,1)) #неправильные кавычки, (округление)
+di=a*sqrt(2) #неправильная запись функции
 print("Ruudu diagonaal", round(di,2))
 print()
 print("Ristkuliku karakteristikud") #лишняя закрывающая скобка
 b=float(input("Sisesta ristkuliku 1. kulje pikkus => ")) #float
 c=float(input("Sisesta ristkuliku 2. kulje pikkus => ")) #float
 S=b*c
-print("Ristkuliku pindala", round(S,1)) #неправильные кавычки #
+print("Ristkuliku pindala", round(S,1)) #не было открывающих кавычек, (округление)
 P=2*(b+c) #не было знака умножения
-print("Ristkuliku umbermoot", round(P,1)) #
-di=sqrt(b**2+c**2) 
-print("Ristkuliku diagonaal", round(di,1)) #не было закрывающей скобки
+print("Ristkuliku umbermoot", round(P,1)) #(округление)
+di=sqrt(b**2+c**2) #неправильная запись функции, не было 2го знака умножения х2
+print("Ristkuliku diagonaal", round(di,1)) #не было закрывающей скобки, (округление)
 print()
 print("Ringi karakteristikud")
-r=input("Sisesta ringi raadiusi pikkus => ") #неправильно стоят квычки, лишняя закрывающая скобка
+r=float(input("Sisesta ringi raadiusi pikkus => ")) #неправильно стоят квычки, лишняя закрывающая скобка, float
 d=2*r #не было знака умножения
 print("Ringi labimoot", d) #не было запятой
-S=pi*r**2 
-print("Ringi pindala hjghdfgdjkghkjfffffffffffffffffffffff", round(S,2))#
-C=2*pi*r #не было запятой, лишние скобки
-print("Ringjoone pikkus", round(C,2)) #не было закрывающей скобки
+S=pi*r**2 #не было 2го знака умножения, лишние скобки
+print("Ringi pindala", round(S,2)) #(округление)
+C=2*pi*r #не было знака умножения, лишние скобки
+print("Ringjoone pikkus", round(C,2)) #не было закрывающей скобки, (округление)
---
+++ pohikonskruktsioonid.py
@@ -0,0 +1,109 @@
+
+# print("Tere maailm!")
+# nimi=input("Mis on sinu nimi?")
+# vanus=int(input("Kui vana sa oled?")) #kommentaar
+
+# print("Tere "+nimi+"! Sa oled "+str(vanus)+" aastat vana.")
+# print("Tere",nimi,"! Sa oled",vanus,"aastat vana.")
+# print("Tere {0}! Sa oled {1} aastat vana.".format(nimi,vanus))
+
+# print(type(nimi))
+# print(type(vanus))
+
+# arv1=float(input("Arv 1: "))
+# t=input("Tehe: ")
+# arv2=float(input("Arv 2: "))
+# vastus=eval(str(arv1)+t+str(arv2))
+# print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+
+
+##2
+# vanus=18
+# eesnimi="Jaak"
+# pikkus=16.5
+# kas_kaib_koolis=True
+
+# print("Muutuja",vanus,"on",type(vanus))
+# print("Muutuja",eesnimi,"on",type(eesnimi))
+# print("Muutuja",pikkus,"on",type(pikkus))
+# print("Muutuja",kas_kaib_koolis,"on",type(kas_kaib_koolis))
+
+
+##3
+#from random import * #koik funktsioonid mis on moodulis
+#kogus=randint(1,100)
+#print("Kokku on",kogus,"kommid")
+#mitu=int(input("Mitu kommi tahad votta?"))
+#kogus=kogus-mitu
+#print("Laua peal on",kogus,"kommid")
+
+
+##4
+# from math import *
+# l=float(input("Umbermoot: "))
+# d=round(l/pi,2) #3.14...
+# print("Labimoot=",d)
+
+
+##5
+# from math import *
+# N=float(input("N: "))
+# M=float(input("M: "))
+# diag=sqrt(M**2+N**2)
+# print("diag=",round(diag,2))
+
+
+##6
+# aeg = float(input("Mitu tundi kulus soiduks? "))
+# teepikkus = float(input("Mitu kilomeetrit soitsid? "))
+# kiirus = aeg / teepikkus
+
+# print("Sinu kiirus oli " + str(kiirus) + " km/h")
+
+
+##7
+# arv1=int(input("arv1: "))
+# arv2=int(input("arv2: "))
+# arv3=int(input("arv3: "))
+# arv4=int(input("arv4: "))
+# arv5=int(input("arv5: "))
+# keskmine=(arv1+arv2+arv3+arv4+arv5)/5
+# print("avg=",round(avg,2))
+
+
+from random import randint
+
+a1=randint(1,50)
+a2=randint(1,50)
+a3=randint(1,50)
+a4=randint(1,50)
+a5=randint(1,50)
+keskmine=(a1+a2+a3+a4+a5)/5
+print("Arvud=",round(avg,2)) #
+
+
+##8
+# print("   @..@")
+# print("  (----)")
+# print(" ( \__/ )")
+# print(" ^^ \"\" ^^")
+
+
+# print("@..@".center(10))
+# print("(----)".center(10))
+# print("( \__/ )".center(10))
+# print("^^ \"\" ^^".center(10))
+
+
+##9
+# a=int(input("a: "))
+# b=int(input("b: "))
+# c=int(input("c: "))
+# P=a+b+c
+# print("P=",round(P,2))
+
+
+##10
+# pitsa=12.90
+# hind=12.90/2+12.90*0.1/2
+# print("hind=",round(hind,2))
